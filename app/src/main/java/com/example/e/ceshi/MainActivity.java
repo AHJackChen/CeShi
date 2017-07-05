018be6297a65ebed40ec87a533d3e3c3bbcea6ca
e <851647340@qq.com>
2017-07-05 20:40:23 +0800

    第二次更改后的

--- app/src/main/java/com/example/e/ceshi/MainActivity.java
+++ app/src/main/java/com/example/e/ceshi/MainActivity.java
@@ -11,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("陈庆");
+        System.out.println("张惠行");
+        System.out.println("李开虎");
+        System.out.println("廉锦雪");
+        System.out.println("黄慧");
     }
 }
