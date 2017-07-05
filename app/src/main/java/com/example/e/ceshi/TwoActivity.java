1e500de3f0110e036fecb0187bb5ca4cc1f09e92
jinxue1017 <2468187477@qq.com>
2017-07-05 21:00:39 +0800

    新添加一个类

---
+++ app/src/main/java/com/example/e/ceshi/TwoActivity.java
@@ -0,0 +1,18 @@
+package com.example.e.ceshi;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 创建者： 廉锦雪
+ * 创建时间:2017/7/520:58
+ */
+
+public class TwoActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
